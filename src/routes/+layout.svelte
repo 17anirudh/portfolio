305227537d7e2fc0eb84baf5bf4da220b005a6d8
Anirudh <vedalaanirudh@gmail.com>
2025-09-19 20:49:20 +0530

    added scss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<style lang="scss">
+	div#main-body{
+		background-color: black;
+		color: aliceblue;
+	}
+</style>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div id="main-body">
+	{@render children?.()}
+</div>
